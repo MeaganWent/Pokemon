3d06972ba91d3978e60732f7327c52596afc4784
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2024-04-22 14:45:44 +0000

    maybee

--- login.php
+++ login.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   if ($user) {
     login($username);                               
-    header('Location: index.php');
+    header('Location: profile.php');
     exit;   
   } else {
     $login_err = "Invalid username or password";
