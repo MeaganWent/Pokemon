32977425a9c9511df8f357901782de076ddd8ccb
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2024-04-22 14:40:31 +0000

    login

--- login.php
+++ login.php
@@ -1,74 +1,71 @@
 <?php
+require 'includes/session.php';
 
-  require 'includes/session.php';
+if ($logged_in) {   
+  header('Location: index.php'); 
+  exit;
+}    
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-  if ($logged_in) {   
-    header('Location: index.php'); 
-    exit;
-  }    
+  $user = authenticate($pdo, $username, $password);
 
-
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-
-    $user = authenticate($pdo, $username, $password);
-
-    if ($user) {
-      login($username);                               
-      header('Location: index.php');
-      exit;   
-    }
+  if ($user) {
+    login($username);                               
+    header('Location: index.php');
+    exit;   
+  } else {
+    $login_err = "Invalid username or password";
   }
-
+}
 ?> 
 
-<!DOCTYPE>
+<!DOCTYPE html>
 <html>
 
-  <head>
-    <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Cosmic Horoscopes</title>
-  </head>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cosmic Horoscopes</title>
+</head>
 
-  <body>
-
-    <header>
-      <div>
-        <div>
-            </div>
-
-            <nav>
-              <ul>
-                <li><a href="index.php">Horoscopes</a></li>
-              </ul>
-          </nav>
-        </div>
-
-        <div>
-          <ul>
-            <li><?= $logged_in ? '<a href="logout.php">Log Out</a>' : '<a href="login.php">Log In</a>'; ?></li>
-          </ul>
-        </div>
-    </header>
-
+<body>
+  <header>
     <div>
-      <h1>Log In</h1>
-      <hr />
-      <br />
-      
-      <form method="POST" action="login.php">
-        Username: <input type="username" name="username"><br>
-        Password: <input type="password" name="password"><br>
-        <input type="submit" value="Log In">
-      </form>
+      <div>
+      </div>
 
+      <nav>
+        <ul>
+          <li><a href="index.php">Horoscopes</a></li>
+        </ul>
+      </nav>
     </div>
 
-  </body>
+    <div>
+      <ul>
+        <li><?= $logged_in ? '<a href="logout.php">Log Out</a>' : '<a href="login.php">Log In</a>'; ?></li>
+      </ul>
+    </div>
+  </header>
+
+  <div>
+    <h1>Log In</h1>
+    <hr />
+    <br />
+    <?php if(isset($login_err)) { ?>
+      <p style="color: red;"><?php echo $login_err; ?></p>
+    <?php } ?>
+    
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+      Username: <input type="text" name="username"><br>
+      Password: <input type="password" name="password"><br>
+      <input type="submit" value="Log In">
+    </form>
+
+  </div>
+
+</body>
 </html>
-
-
